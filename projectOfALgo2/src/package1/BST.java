1891e01c37eedff1ab87906b25c96a6089c41a6b
Y <Y@DESKTOP-O55PM4U>
2023-05-05 23:35:38 +0300

    Merge branch 'master' of https://github.com/yousefrz173/homeworkOfAlgo2.git

--- projectOfALgo2/src/package1/BST.java
+++ projectOfALgo2/src/package1/BST.java
@@ -34,8 +34,13 @@ public class BST {
 		return root;
 	}
 
-	public int inser() {
+	public int inserddd() {
 		return -2;
+	}
+	public void hhh() {
+		System.out.println();
+		System.out.println();
+		System.out.println();
 	}
 	public void Insert(int value) { // repetitive
 		Node node = new Node(value);
